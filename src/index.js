7e6979a16bb1f8d8b84e850af5389484a91fe701
Luiz Fernando <devfernandoof@gmail.com>
2020-09-16 21:24:19 -0300

    🔥 final commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import HomeView from "./views/Home.view";
 import "./index.css";
+
 import { Provider } from "react-redux";
-import store from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistedStore } from "./store/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HomeView />
+      <PersistGate persistor={persistedStore}>
+        <HomeView />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,20 @@
 import { createStore, combineReducers } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import productsReducer from "./Products/Products.reducer";
 
 const rootReducer = combineReducers({
   products: productsReducer,
 });
 
-const store = createStore(rootReducer);
+const persistedReducer = persistReducer(
+  {
+    key: "root",
+    storage,
+  },
+  rootReducer
+);
 
-export default store;
+export const store = createStore(persistedReducer);
+export const persistedStore = persistStore(store);
